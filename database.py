e4101a464f608e3dad1e15ad960e964113ee1523
JoaoCostaTI <joao.costapp@outlook.com>
2025-12-19 06:24:33 -0300

    as

--- database.py
+++ database.py
@@ -15,6 +15,8 @@ class BancoDeDados():
     def criar_tabelas(self):
         self.conectar()
         cursor = self.conexao.cursor()
+        #Respeitar as conexões entre as tabelas
+        cursor.execute('PRAGMA foreign_keys = ON;')
 
         #Tabela Clientes
         cursor.execute('CREATE TABLE IF NOT EXISTS clientes(' \
@@ -30,11 +32,35 @@ class BancoDeDados():
         'preco real,' \
         'estoque integer)')
 
+        #tabela Vendas
+        cursor.execute("""
+    create table if not exists vendas(
+        id integer primary key autoincrement,
+        cliente_id integer,
+        data text,
+        valor_real real,
+        FOREIGN KEY(cliente_id) REFERENCES clientes(id)            
+        )
+""")
+        #tabela itens_venda
+        cursor.execute("""
+    create table if not exists itens_venda(
+                       id integer primary key autoincrement,
+                       venda_id integer,
+                       produto_id integer,
+                       quantidade integer,
+                       valor_unitario real,
+                       FOREIGN KEY(venda_id) REFERENCES vendas(id),
+                       FOREIGN KEY(produto_id) REFERENCES produtos(id)
+                       )
+""")
+
         self.conexao.commit()
         self.desconectar()
+
 
 #Testando a classe Banco de Dados
 if __name__ == "__main__":
     banco = BancoDeDados('techvendas.db')
     banco.criar_tabelas()
-    
+
